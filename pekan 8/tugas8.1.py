2361d954e20b42fc1aaa07faf4c28c3cbf265370
Mufadhal27 <146080291+Mufadhal27@users.noreply.github.com>
2023-11-22 10:38:34 +0700

    Add files via upload

---
+++ pekan 8/tugas8.1.py
@@ -0,0 +1,8 @@
+def profil(nama,alamat,gender,umur,hoby):
+    print("Nama \t:", nama)
+    print("Alamat \t:", alamat)
+    print("Gender \t:", gender)
+    print("Umur \t:", umur)
+    print("Hoby \t:", hoby)
+
+profil("Fadhal", "Jakarta", "Laki-laki", 20, "Musik")
---
+++ pekan 8/tugas8.3.py
@@ -0,0 +1,6 @@
+def cari_ganjil(batas):
+    for i in range(batas + 1):
+        if i % 2 != 0:
+            print("angka nilai ganjil", i)
+
+cari_ganjil(100)
